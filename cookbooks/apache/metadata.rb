30c38682de51db63367d77fbdfc9231aeb8665a1
John Varghese <lookitup4me@gmail.com>
2015-03-31 12:10:21 -0700

    updated metadata to get rid of a foodcritic error

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -1,6 +1,6 @@
 name             'apache'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'yuzu'
+maintainer_email 'jv@yuzu.com'
 license          'All rights reserved'
 description      'Installs/Configures apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
